f133e3e633fe5e911fc7c3e7eaf6461ecf877ef5
jordanmowry <jordan.mowry@gmail.com>
2025-10-06 16:39:54 -0500

    feat: implemented /uses

---
+++ app/components/Tool.vue
@@ -0,0 +1,23 @@
+<template>
+  <Card as="li">
+    <CardTitle as="h3" :href="href">
+      {{ title }}
+    </CardTitle>
+    <CardDescription>
+      <slot />
+    </CardDescription>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import Card from './Card.vue';
+import CardTitle from './CardTitle.vue';
+import CardDescription from './CardDescription.vue';
+
+interface ToolProps {
+  title: string;
+  href?: string;
+}
+
+defineProps<ToolProps>();
+</script>
---
+++ app/components/ToolsSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <Section :title="title">
+    <ul role="list" class="space-y-16">
+      <slot />
+    </ul>
+  </Section>
+</template>
+
+<script setup lang="ts">
+import Section from './Section.vue';
+
+interface ToolsSectionProps {
+  title: string;
+}
+
+defineProps<ToolsSectionProps>();
+</script>
