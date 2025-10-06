340ecc3942dcc4c281318bb060a7e4436b1ef804
jordanmowry <jordan.mowry@gmail.com>
2025-10-06 12:39:13 -0500

    feat: implemented /about

---
+++ app/components/AboutSocialLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <li :class="['flex', className]">
+    <NuxtLink
+      :to="href"
+      class="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+    >
+      <component
+        :is="icon"
+        class="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500"
+      />
+      <span class="ml-4">
+        <slot />
+      </span>
+    </NuxtLink>
+  </li>
+</template>
+
+<script setup lang="ts">
+interface AboutSocialLinkProps {
+  href: string;
+  icon: any;
+  className?: string;
+}
+
+defineProps<AboutSocialLinkProps>();
+</script>
